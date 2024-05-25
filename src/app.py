f5890ffbee7237b0549653085c33158e52486bda
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-05-25 01:42:25 +0000

    python todos api

---
+++ src/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = [
+    {
+            "label": "Wash Car",
+            "done": False
+    },
+    {
+          "label": "Do homework",
+          "done": False
+    }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+        json_text =jsonify(todos)
+        return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+      request_body = request.json
+      todos.append(request_body)
+      print("Incoming request with the following body", request_body)
+      return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+      selected_item = todos.pop(position)
+      print("This is the position to delete:", position)
+      print (f'Todo {selected_item} deleted from the list.')
+      return jsonify(todos)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
+
